44a478a421a6f2dca1118f06be717fc7f38c7cf5
Kang Budi <47937538+BudiBelajar@users.noreply.github.com>
2020-02-26 07:54:07 +0700

    Add files via upload

---
+++ Praktikum 4/tugas3_in.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contohpembuatan Form</title>
+</head>
+<body>
+<form action="tugas3.php" method="POST">
+	<table>
+		<tr>
+			<td>Nama</td>
+			<td><input type="text" name="nama"></td>
+		</tr>
+
+		<tr>
+			<td>Alamat</td>
+			<td><textarea name="alamat"></textarea></td>
+		</tr>
+
+		<tr>
+			<td>Jenis Kelamin</td>
+			<td>
+				<select name="jk">
+					<option value="L">Laki-laki</option>
+					<option value="P">Perempuan</option>
+				</select>
+			</td>
+		</tr>
+
+		<tr>
+			<td>Email</td>
+			<td><input type="text" name="Email"></td>
+		</tr>
+
+		<tr>
+			<td>Hitung</td>
+			<td><?php $a = rand(1,10); $b = rand (1,10);?>
+			<input type="hidden" name="var_a" value="<?php echo $a; ?>">	
+			<input type="hidden" name="var_b" value="<?php echo $b; ?>">	
+			<?php echo $a; echo "*"; echo $b; ?>		
+			</td>
+		</tr>
+
+		<tr>
+			<td>Jawab</td>
+			<td><input type="text" name="Jawab"></td>
+		</tr>
+
+			<td>
+				<input type="submit" name="submit">
+				<input type="reset" name="reset">
+			</td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
